616052b38f677fce6b22c27cdc4184a99093af20
Ian McGreal <imcgreal10@gmail.com>
2026-04-18 15:06:22 -0400

    Created actions.py to handle SQL parameter inputs for each menu option and to make the calls to execute_query in db.py

---
+++ actions.py
@@ -0,0 +1,28 @@
+from db import execute_query
+
+def option_1(cursor):
+    print("Yay!")
+    # rows = execute_query(cursor, "SQL goes here")
+    # for r in rows:
+    #     print(r)
+
+def option_2(cursor):
+    print("Yay!")
+
+def option_3(cursor):
+    print("Yay!")
+
+def option_4(cursor):
+    print("Yay!")
+
+def option_5(cursor):
+    print("Yay!")
+
+def option_6(cursor):
+    print("Yay!")
+
+def option_7(cursor):
+    print("Yay!")
+
+def option_8(cursor):
+    print("Yay!")
