5cc00df22c261ff064d453c1e34e1dd98ba9f983
andyz0628 <andrewzhou0628@gmail.com>
2026-04-19 12:20:56 -0400

    Added options 1 and 2

--- menu.py
+++ menu.py
@@ -1,10 +1,10 @@
 from actions import option_1, option_2, option_3, option_4, option_5, option_6, option_7, option_8
 
 MENU = {
-    "1": ("Option 1 description", option_1),
-    "2": ("Option 2 description", option_2),
-    "3": ("Option 3 description", option_3),
-    "4": ("Option 4 description", option_4),
+    "1": ("See a chronological log of all automated trading actions generated for a stock.", option_1),
+    "2": ("Group stocks by ticker and calculate their maximum and minimum RSI values over a specific year to find the most volatile assets.", option_2),
+    "3": ("Find days where a 'Buy' signal triggered, but only if the trading volume on that specific day was strictly greater than that exact stock's average daily volume for a specific year.", option_3),
+    "4": ("See all price action and calculated indicators for a stock in a defined date range", option_4),
     "5": ("Option 5 description", option_5),
     "6": ("Option 6 description", option_6),
     "7": ("Option 7 description", option_7),
